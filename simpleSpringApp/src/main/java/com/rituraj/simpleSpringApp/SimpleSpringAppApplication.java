d398eb476d68d3d775adca556c2b706cea6cd4f5
Rituraj2610 <26rituraj@gmail.com>
2025-04-18 10:13:15 +0530

    [Rituraj] ADD . UC1_Assignment2

--- simpleSpringApp/src/main/java/com/rituraj/simpleSpringApp/SimpleSpringAppApplication.java
+++ simpleSpringApp/src/main/java/com/rituraj/simpleSpringApp/SimpleSpringAppApplication.java
@@ -1,13 +1,22 @@
 package com.rituraj.simpleSpringApp;
 
+
+import com.rituraj.simpleSpringApp.controller.Home;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SimpleSpringAppApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SimpleSpringAppApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringAppApplication.class, args);
+		logger.debug("Welcome");
+		ApplicationContext context = (ApplicationContext) SpringApplication.run(SimpleSpringAppApplication.class, args);
+		logger.debug("Checking context : {}", context.getBean(Home.class));
+
 	}
 
 }
